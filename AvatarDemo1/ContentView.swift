108820918206eccdd70f5f98d05ab391b679a797
ADINA GROSS <158787499+agross25@users.noreply.github.com>
2025-03-11 07:23:33 -0400

    first commit

--- AvatarDemo1/ContentView.swift
+++ AvatarDemo1/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isHappy = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: isHappy ? "face.smiling" : "face.frown")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.yellow)
+            
+            Button("Change Mood") {
+                isHappy.toggle()
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
