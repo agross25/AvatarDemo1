32fb3a4c897255420801fca10da374021e5425df
ADINA GROSS <158787499+agross25@users.noreply.github.com>
2025-03-11 06:36:10 -0400

    Initial Commit

---
+++ AvatarDemo1/AvatarDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarDemo1App.swift
+//  AvatarDemo1
+//
+//  Created by Adina Gross on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AvatarDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvatarDemo1Tests/AvatarDemo1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarDemo1Tests.swift
+//  AvatarDemo1Tests
+//
+//  Created by Adina Gross on 3/11/25.
+//
+
+import Testing
+@testable import AvatarDemo1
+
+struct AvatarDemo1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
